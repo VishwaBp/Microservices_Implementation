bddc271497db89468f57a634a71b9510c6c7a524
Vishwanath <Vishwanath26287@gmail.com>
2023-12-05 16:39:37 +0530

    Microservices Implementation 1.

---
+++ src/test/java/com/vishwanath/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwanath.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
